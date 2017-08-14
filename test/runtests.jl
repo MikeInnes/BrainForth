b6fe3ed984c4319d98adbe06fda1bb4c34b2d525
Mike J Innes <mike.j.innes@gmail.com>
2017-08-14 16:57:24 +0100

    testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,37 +3,40 @@ using Base.Test
 
 @testset "BrainForth" begin
 
-@test stack(@run([8, 5, dup]))  == [8, 5, 5]
-@test stack(@run([8, 5, swap])) == [5, 8]
-@test stack(@run([8, 5, drop])) == [8]
+@testset "Basics" begin
+  @test stack(@run([8, 5, dup]))  == [8, 5, 5]
+  @test stack(@run([8, 5, swap])) == [5, 8]
+  @test stack(@run([8, 5, drop])) == [8]
 
-@test stack(@run([0, !]))     == [1]
-@test stack(@run([8, !]))     == [0]
-@test stack(@run([8, 5, +]))  == [13]
-@test stack(@run([8, 5, -]))  == [3]
-@test stack(@run([8, 5, *]))  == [40]
+  @test stack(@run([0, !]))     == [1]
+  @test stack(@run([8, !]))     == [0]
+  @test stack(@run([8, 5, +]))  == [13]
+  @test stack(@run([8, 5, -]))  == [3]
+  @test stack(@run([8, 5, *]))  == [40]
 
-@test stack(@run([8, 5, [1, +], dip]))         == [9, 5]
-@test stack(@run([8, 5, nip]))                 == [5]
-@test stack(@run([8, 5, over]))                == [8, 5, 8]
-@test stack(@run([8, 5, [sq], keep]))          == [8, 25, 5]
-@test stack(@run([1, 2, 3, rotl]))             == [2, 3, 1]
-@test stack(@run([8, 5, [1, +], [1, -], bi]))  == [8, 6, 4]
-@test stack(@run([8, 5, [1, +], [1, -], bi_])) == [9, 4]
-@test stack(@run([8, 5, [1, +], bia]))         == [9, 6]
+  @test stack(@run([8, 5, [1, +], dip]))         == [9, 5]
+  @test stack(@run([8, 5, nip]))                 == [5]
+  @test stack(@run([8, 5, over]))                == [8, 5, 8]
+  @test stack(@run([8, 5, [sq], keep]))          == [8, 25, 5]
+  @test stack(@run([1, 2, 3, rotl]))             == [2, 3, 1]
+  @test stack(@run([8, 5, [1, +], [1, -], bi]))  == [8, 6, 4]
+  @test stack(@run([8, 5, [1, +], [1, -], bi_])) == [9, 4]
+  @test stack(@run([8, 5, [1, +], bia]))         == [9, 6]
 
-@test stack(@run([5, [[1, +], call, [1, +], call], call])) == [7]
+  @test stack(@run([5, [[1, +], call, [1, +], call], call])) == [7]
 
-@test stack(@run([10, 1, [1, +], [1, -], iff])) == [11]
-@test stack(@run([10, 0, [1, +], [1, -], iff])) == [ 9]
-@test stack(@run([10, 1, [[[1, +], call], [[1, -], call], iff], call])) == [11]
+  @test stack(@run([10, 1, [1, +], [1, -], iff])) == [11]
+  @test stack(@run([10, 0, [1, +], [1, -], iff])) == [ 9]
+  @test stack(@run([10, 1, [[[1, +], call], [[1, -], call], iff], call])) == [11]
+end
 
-@bf factorial = [dup, 1, ==, [dup, 1, -, factorial, *], unless]
-@test stack(@run([5, factorial])) == [120]
+@testset "Recursion" begin
+  @bf factorial = [dup, 1, ==, [dup, 1, -, factorial, *], unless]
+  @test stack(@run([5, factorial])) == [120]
 
-@bf fib = [dup, [1, ==], [0, ==], bi, or,
-                [[1, -, fib], [2, -, fib], bi, +], unless]
-
-@test stack(@run([7, fib])) == [13]
+  @bf fib = [dup, [1, ==], [0, ==], bi, or,
+                  [[1, -, fib], [2, -, fib], bi, +], unless]
+  @test stack(@run([7, fib])) == [13]
+end
 
 end
