bb287cd921c37801b1a1173003ee9f36668a31c3
Mike J Innes <mike.j.innes@gmail.com>
2017-08-15 20:55:00 +0100

    organise

--- src/array.jl
+++ src/array.jl
@@ -1,6 +1,8 @@
 lower(c::Char) = lower(Int(c))
 lower(s::String) = lower(Word([reverse(s)..., length(s)]))
 
+# Basic array ops
+
 @bf nil = [0]
 @bf push = [swap, 1, +]
 @bf pop = [1, -, swap]
@@ -8,12 +10,32 @@ lower(s::String) = lower(Word([reverse(s)..., length(s)]))
 @bf length = [dup]
 @bf isempty = [length, !]
 
+# Array-stack ops
+
+@bf pushe = [[isempty], dip, swap,
+             [nip, 1], [[pop], dip, swap, [pushe], dip, push], iff]
+
+@bf swapvb = [pushe, 1, -]
+
+@bf dipv = [[isempty], dip, swap,
+            [dip], [[pop], dip, swap, [dipv], dip, push], iff]
+
+@bf swapvv_ = [isempty, [drop], [pop, [swapvv_], dip, swapvb], iff]
+@bf swapvv = [[swapvv_], keep, swapvb]
+
+@bf dupv = [isempty, [dup],
+            [pop, [dupv], dip, # xs, ys, x
+             [swapvb, [push], dipv], keep, # xs:x, ys, x
+             push], # xs:x, ys:y
+            iff]
+
+# Array lib
+
 @bf range = [[[dup, 1, !=], [dup, 1, -], loop], keep]
 
 @bf cat = [isempty, [drop], [pop, [cat], dip, push], iff]
 
-@bf dipv = [[isempty], dip, swap,
-            [dip], [[pop], dip, swap, [dipv], dip, push], iff]
+@bf reverse = [isempty, [pop, [reverse], dip, pushe], unless]
 
 @bf each = [[isempty], dip, swap,
             [drop, drop], [[pop], dip, [call], keep, each], iff]
@@ -26,20 +48,6 @@ lower(s::String) = lower(Word([reverse(s)..., length(s)]))
 @bf fold = [[isempty], dip2, rotl,
             [drop, nip], [fold_], iff]
 
-@bf pushe = [[isempty], dip, swap,
-             [nip, 1], [[pop], dip, swap, [pushe], dip, push], iff]
-
-@bf reverse = [isempty, [pop, [reverse], dip, pushe], unless]
-
-@bf swapvb = [pushe, 1, -]
-@bf swapvv_ = [isempty, [drop], [pop, [swapvv_], dip, swapvb], iff]
-@bf swapvv = [[swapvv_], keep, swapvb]
-
-@bf dupv = [isempty, [dup],
-            [pop, [dupv], dip, # xs, ys, x
-             [swapvb, [push], dipv], keep, # xs:x, ys, x
-             push], # xs:x, ys:y
-            iff]
 
 @bf sum = [0, [+], fold]
 @bf prod = [1, [*], fold]
